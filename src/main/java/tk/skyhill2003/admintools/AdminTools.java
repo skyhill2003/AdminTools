aed6500a0fcb373a5154b838ffe8a51175e3dfae
skyhill2003 <skyhill2003@gmail.com>
2022-01-16 11:52:49 +0100

    first release - start release

---
+++ src/main/java/tk/skyhill2003/admintools/AdminTools.java
@@ -0,0 +1,16 @@
+package tk.skyhill2003.admintools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdminTools extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
